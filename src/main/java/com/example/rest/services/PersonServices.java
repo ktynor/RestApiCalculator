379fd1e97838b142c2213fd753adcd825880111d
ktynor <kt9@tlen.pl>
2021-01-29 20:07:12 +0000

    Returning Objects List

--- src/main/java/com/example/rest/services/PersonServices.java
+++ src/main/java/com/example/rest/services/PersonServices.java
@@ -3,6 +3,8 @@ package com.example.rest.services;
 import com.example.rest.model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Service
@@ -10,12 +12,32 @@ public class PersonServices {
 
     private final AtomicLong counter = new AtomicLong();
 
-    public Person findById(String id) {
+    public List<Person> findAll() {
+        List<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 8; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+        return persons;
+    }
+
+    public Person findById(String id) { // ERROR! creates and returns always a new person!
+        // TODO implement a correct method
         Person person = new Person();
         person.setId(counter.incrementAndGet());
         person.setFirstName("Leandro");
         person.setLastName("Costa");
         person.setAddress("Uberlândia - Minas Gerais -Brasil");
+        person.setGender("Male");
+        return person;
+    }
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Leandro" + i);
+        person.setLastName("Costa" + i);
+        person.setAddress("Uberlândia - Minas Gerais -Brasil" + i);
         person.setGender("Male");
         return person;
     }
